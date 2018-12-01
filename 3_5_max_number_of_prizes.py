ec484dc56a846085e1508acedef4320e5bf69c55
dimau777@gmail.com <Dimau7.toporovI2.valenka>
2018-12-01 20:14:04 +0300

    new functions

--- 3_5_max_number_of_prizes.py
+++ 3_5_max_number_of_prizes.py
@@ -16,16 +16,16 @@ def greedy_get_max_number_of_prizes(n):
     return len(prizes_list), prizes_list
 
 
-# with sys.stdin as f:
-#     n = int(f.readline())
-#     my_algorithm_result_k, my_algorithm_result_prizes = greedy_get_max_number_of_prizes(n)
-#     print(my_algorithm_result_k)
-#     my_algorithm_result_prizes_string = [str(item) for item in my_algorithm_result_prizes]
-#     print(" ".join(my_algorithm_result_prizes_string))
-
-while True:
-    n = random.randint(0, 1000000000)
-    # n = 2
-    print("n = " + str(n))
+with sys.stdin as f:
+    n = int(f.readline())
     my_algorithm_result_k, my_algorithm_result_prizes = greedy_get_max_number_of_prizes(n)
-    print("Result greedy algorithm: " + str(my_algorithm_result_k) + " all prizes: " + str(my_algorithm_result_prizes))
+    print(my_algorithm_result_k)
+    my_algorithm_result_prizes_string = [str(item) for item in my_algorithm_result_prizes]
+    print(" ".join(my_algorithm_result_prizes_string))
+
+# while True:
+#     n = random.randint(0, 1000000000)
+#     # n = 2
+#     print("n = " + str(n))
+#     my_algorithm_result_k, my_algorithm_result_prizes = greedy_get_max_number_of_prizes(n)
+#     print("Result greedy algorithm: " + str(my_algorithm_result_k) + " all prizes: " + str(my_algorithm_result_prizes))
